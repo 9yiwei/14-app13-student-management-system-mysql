1600f78ea909fc7d31c3a06a5b5fe199e97f4a96
Howrad <9yiwei@gmail.com>
2023-10-27 08:58:18 +0800

    Day 46: inserting new records

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
 
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
@@ -34,7 +35,49 @@ class MainWindow(QMainWindow):
                 self.table.setItem(row_number, col_number, QTableWidgetItem(str(data)))
         connect.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
 
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        submit = QPushButton("Register")
+        submit.clicked.connect(self.add_student)
+        layout.addWidget(submit)
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        connect = sqlite3.connect("database.db")
+        cursor = connect.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)", (name, course, mobile))
+        connect.commit()
+        cursor.close()
+        connect.close()
+        main_window.load_data()
 
 
 app = QApplication(sys.argv)
